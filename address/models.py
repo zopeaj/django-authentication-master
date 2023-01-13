8f6708a8e0ab48518817dee7511c03cf5f9a144b
Ezekiel <zopeaj@gmail.com>
2023-01-13 08:41:06 +0100

    account/models.py commited

--- address/models.py
+++ address/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from account.models import Account
 
 # Create your models here.
+class Address(models.Model):
+    user = models.ManyToOne(Account, on_delete=models.CASCADE)
+    address_book = models.CharField()
